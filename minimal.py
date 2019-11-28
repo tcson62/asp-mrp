34cabfaa4870f2fe671815a9e28a77bd105d0378
Son Tran <tson@cs.nmsu.edu>
2019-11-27 22:51:25 -0700

    Update 11/25

--- minimal.py
+++ minimal.py
@@ -5,48 +5,76 @@ import string
 import sys 
 import clingo 
 
-#from clingo import *
-
-global debug  
-
-def print_conf(conf, ident):
-    for key in conf.keys:
-        print ("====>",key)    
-        #  if (key == "heuristic"): 
-        subconf = getattr(conf, key)
-        if isinstance(subconf, clingo.Configuration):
-            label = key
-            if (len(subconf) >= 0):
-                label += "[0.." + str(len(subconf)) + "]"
-            print ("{0}{1} - {2}".format(ident, label, getattr(conf, "__desc_" + key)))
-            print_conf(subconf, "  " + ident + label + ".")
-        else:
-            print ("{0}{1}[={2}] - {3}".format(ident, key, subconf, getattr(conf, "__desc_" + key)))
-
+global debug                   
+global curr_as            # current answer set 
+global minLength       # minimal plan length 
+global curr_plan         # current plan 
 
 def all_model(m) :
-    print ("Answer: {}".format(m))   
+    global curr_as 
+    global debug 
+    global curr_plan
+    
+    if (debug) : print ("Answer: {}".format(m))   
+    
+    curr_as = m.symbols(shown=True)
+    
+    if (debug) :  print ("List of shown elements: {}".format(curr_as))   
+    
+    for x in range(0, len(curr_as)) : 
+        if (curr_as[x].match("occurs",2)) :
+             print (x, ':', curr_as[x], ' --- ', curr_as[x].arguments)     
+             curr_plan.append(curr_as[x])
+
+
+def computeMax(m):
+    global curr_as
+    global minLength
+    curr_as = m.symbols(atoms=True)  
+    if (debug) :  print ("List of all elements: {}".format(curr_as))        
+    for x in range(0, len(curr_as)) : 
+         if (curr_as[x].match("maxTime",1)) :
+             print (x, ':', curr_as[x], ' --- ', curr_as[x].arguments)     
+             minLength = curr_as[x].arguments[0]  
 
 
 def main(prg):
     global debug 
+    global curr_as
+    global minLength 
+    global curr_plan 
     
     debug = True 
+    curr_plan = []
 
     start_time = datetime.datetime.now()  
     
+    prg.ground([("robot",[])]) 
+    
+    prg.solve(None, on_model=computeMax)
+
+    print ("Minimal plan length: {}".format(minLength)) 
+
     prg.ground([("base",[])])
     
+    if (debug) :  print(prg.configuration.solver.heuristic, "")
+    
     prg.configuration.solver.heuristic="Domain"
-    
-    blub = prg.configuration.solver
 
-    print_conf(prg.configuration, "")
-
-    
-   
+    if (debug) :  print(prg.configuration.solver.heuristic, "")
+     
+    # Need to do 
+    #     get all the actions  
+    #     check for noop 
+    #     if noop does not exist then done 
+    #     else change the programs  
+      
     prg.solve(None, on_model=all_model)
     
+    if (debug) : print ("Signature: {} ".format( prg.symbolic_atoms.signatures))
+    
+    print ("Current plan :".format(curr_plan))    
+     
     end_time = datetime.datetime.now()
     
     elapsed = end_time - start_time
